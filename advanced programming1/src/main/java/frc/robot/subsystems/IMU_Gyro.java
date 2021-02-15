fb1277d3d04279754d87812ef61d15988e9d9eb4
Slink <hoodie888@outlook.com>
2021-02-15 10:32:07 -0600

    imu subsystem working maybe?

--- advanced programming1/src/main/java/frc/robot/subsystems/IMU_Gyro.java
+++ advanced programming1/src/main/java/frc/robot/subsystems/IMU_Gyro.java
@@ -7,13 +7,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.analog.adis16470.frc.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.AnalogGyro;
 import frc.robot.Constants;
-import frc.robot.Robot;
-import edu.wpi.first.wpilibj.TimedRobot;
 
 public class IMU_Gyro extends SubsystemBase {
   private ADIS16470_IMU imu = new ADIS16470_IMU();
-  private AnalogGyro imu_gyro = new AnalogGyro(Constants.gyro_port);
-  private double dt;
+  //private AnalogGyro imu_gyro = new AnalogGyro(Constants.gyro_port);      //<<< not sure if necessary
   
   
 
@@ -25,58 +22,61 @@ public class IMU_Gyro extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-
-  public void accelDist_v1(){
+  /**THIS METHOD IS MARKED AS V1 AS IT WAS COPIED FROM PREXISTING CODE AND MAY NEED CHANGES TO WORK PROPERLY IN SUBSYSTEM FORMAT    
+  Returns array of doubles correstponding to the distance moved within the time period. 
+  The parameter "period_time" refers to the time between periods which can be gotten with "getPeriod()" (Unable to use this method outside of Robot.java). */
+  public double[] accelDist_v1(double period_time){
     double[] pos = {0.0, 0.0, 0.0};
     double accelX[] = {0,0,0,0,0,0,0,0,0,0};
     double accelY[] = {0,0,0,0,0,0,0,0,0,0};
     double accelZ[] = {0,0,0,0,0,0,0,0,0,0};
-    dt = TimedRobot.getPeriod();
     int i = 0;
-    double velocityX = 0;
-    double velocityY = 0;
-    double velocityZ = 0;
     double distanceX = 0;
     double distanceY = 0;
     double distanceZ = 0;
-    
+    double sum;
+    double dt = period_time;
+
+    //run a loop of ten
+    while (i<=9){
+      //fill up array with values from imu (there are ten spots and the loop runs 10 times)
       accelX[i] += imu.getAccelInstantX() * 9.8;
       accelY[i] += imu.getAccelInstantY() * 9.8;
       accelZ[i] += imu.getAccelInstantZ() * 9.8;
-
-      //X-axis
-      double sum = 0;
-      for(int j=0; j<10; j++){
-        sum += accelX[j];
-      }
-      sum /= 10;
-      velocityX += sum * dt;
-
-      //Y-axis
-      sum = 0;
-      for(int j=0; j<10; j++){
-        sum += accelY[j];
-      }
-      sum /= 10;
-      velocityY += sum * dt;
-
-      //Z-axis
-      sum = 0;
-      for(int j=0; j<10; j++){
-        sum += accelZ[j];
-      }
-      sum /= 10;
-      velocityZ += sum * dt;
-
-      //keep running loops of ten
+      //add one
       i ++;
-      if(i>9) i = 0;
+    }
 
-      //set places in the array that will be returned equal to the values gotten above
-      pos[0] = velocityX;
-      pos[1] = velocityY;
-      pos[2] = velocityZ;
+    //X-axis -> average all values and store in distanceX 
+    sum = 0;
+    for(int j=0; j<10; j++){
+      sum += accelX[j];
+    }
+    sum /= 10;
+    distanceX += sum * dt;
 
+    //Y-axis -> average all values and store in distanceY
+    sum = 0;
+    for(int j=0; j<10; j++){
+      sum += accelY[j];
+    }
+    sum /= 10;
+    distanceY += sum * dt;
+
+    //Z-axis -> average all values and store in disanceZ
+    sum = 0;
+    for(int j=0; j<10; j++){
+      sum += accelZ[j];
+    }
+    sum /= 10;
+    distanceZ += sum * dt;
+
+    //set places in the array that will be returned equal to the values gotten above
+    pos[0] = distanceX;
+    pos[1] = distanceY;
+    pos[2] = distanceZ;  
+
+    //return positions
     return pos;
   }
 
